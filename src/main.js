ec0d5c2c08c0b3178351ab24f5850cfa54397be2
Tommy Leung <me@supertommy.com>
2019-12-30 17:13:34 -0700

    initial commit
    - barebones setup for using Phaser3 with Parceljs
    - runs dev server at localhost:8000
    - builds production files to dist directory

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Phaser from 'phaser'
+
+import HelloWorldScene from './scenes/HelloWorldScene'
+
+var config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 }
+		}
+	},
+	scene: [HelloWorldScene]
+}
+
+export default new Phaser.Game(config)
