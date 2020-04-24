3128904fe576d6a33e457e636dfa9d14e74bf506
Tommy Leung <me@supertommy.com>
2020-04-24 13:49:38 -0600

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,10 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended'
 	],
 	rules: {
-		'@typescript-eslint/explicit-function-return-type': 0
+		'@typescript-eslint/explicit-function-return-type': 0,
+		'@typescript-eslint/ban-ts-ignore': 0,
+		'@typescript-eslint/no-namespace': { 'allowDeclarations': true },
+		'@typescript-eslint/member-delimiter-style': 0,
+		'@typescript-eslint/no-explicit-any': 0
 	}
 }
