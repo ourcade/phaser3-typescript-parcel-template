3c5012584c09e0e43cbb2205fb2e4227f2b30fde
Karol <michkarol.2000@wp.pl>
2021-12-21 17:16:00 +0100

    Add character movement

--- src/scenes/tBOI.ts
+++ src/scenes/tBOI.ts
@@ -2,6 +2,8 @@ import Phaser from 'phaser'
 
 export default class tBOI extends Phaser.Scene
 {
+    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
+    private knight!: Phaser.Physics.Arcade.Sprite
 	constructor()
 	{
 		super('tBOI');
@@ -9,6 +11,7 @@ export default class tBOI extends Phaser.Scene
 
 	preload()
     {
+        this.cursors = this.input.keyboard.createCursorKeys()
 
     }
 
@@ -24,8 +27,24 @@ export default class tBOI extends Phaser.Scene
         walls.setScale(3);
         ground.setScale(3);
 
-        const knight = this.add.sprite(128, 128, 'knight', 'knight_m_idle_anim_f0');
-        knight.setScale(3);
+        this.knight = this.physics.add.sprite(128, 128, 'knight', 'knight_m_idle_anim_f0');
+        this.knight.setScale(3);
+        this.knight.body.setSize(this.knight.width*0.6, this.knight.height*0.2);
+        this.knight.body.setOffset(3, 20);
+
+        this.anims.create({
+            key: 'knight-idle',
+            frames: [{ key: 'knight', frame:'knight_f_idle_anim_f0'}]
+        })
+
+        this.anims.create({
+            key: 'knight-run',
+            frames: this.anims.generateFrameNames('knight', { start:0, end:3, prefix:'knight_f_run_anim_f' }),
+            repeat: -1,
+            frameRate: 15
+        })
+
+        this.physics.add.collider(this.knight, walls);
 
         // const debugGraphics = this.add.graphics().setAlpha(0.7);            // comment/uncomment 
         // walls.renderDebug(debugGraphics,{                                   // here
@@ -36,5 +55,43 @@ export default class tBOI extends Phaser.Scene
 
         console.log("</game>");
 
+    }
+
+    update(t: number, dt: number) {
+        if(!this.cursors || !this.knight)
+        {
+            return
+        }
+
+        const speed = 300;
+        let velocityx: number;
+        let velocityy: number;
+        velocityx = 0;
+        velocityy = 0;
+        this.knight.setVelocity(0, 0);
+        
+        
+        if (this.cursors.left.isDown){
+            velocityx = -speed;
+            this.knight.setFlipX(true);
+        } else if (this.cursors.right.isDown){
+            velocityx = speed;
+            this.knight.setFlipX(false);
+        }
+        if (this.cursors.up.isDown){
+            velocityy = -speed;
+        } else if (this.cursors.down.isDown){
+            velocityy = speed;
+        }
+        if(velocityx != 0 && velocityy != 0){
+            velocityx/=1.42;
+            velocityy/=1.42;    
+        }
+        if(velocityx == 0 && velocityy == 0){
+            this.knight.anims.play('knight-idle');
+        } else{
+            this.knight.anims.play('knight-run', true);
+        }
+        this.knight.setVelocity(velocityx, velocityy);
     }
 }
